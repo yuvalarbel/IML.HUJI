f906ebdd4100bdaf1e9c99aac5af8eca435ae742
Yuval Arbel <yuval.arbel1@mail.huji.ac.il>
2022-04-01 14:48:31 +0300

    implemented linear regression

--- IMLearn/learners/regressors/linear_regression.py
+++ IMLearn/learners/regressors/linear_regression.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from typing import NoReturn
 from ...base import BaseEstimator
+
 import numpy as np
-from numpy.linalg import pinv
+from IMLearn.metrics.loss_functions import mean_square_error
 
 
 class LinearRegression(BaseEstimator):
@@ -49,7 +50,10 @@ class LinearRegression(BaseEstimator):
         -----
         Fits model with or without an intercept depending on value of `self.include_intercept_`
         """
-        raise NotImplementedError()
+        if self.include_intercept_:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        self.coefs_ = np.linalg.pinv(X) @ y
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -65,7 +69,10 @@ class LinearRegression(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        if self.include_intercept_:
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        return X @ self.coefs_
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -84,4 +91,5 @@ class LinearRegression(BaseEstimator):
         loss : float
             Performance under MSE loss function
         """
-        raise NotImplementedError()
+        predictions = self._predict(X)
+        return mean_square_error(y, predictions)
--- IMLearn/metrics/loss_functions.py
+++ IMLearn/metrics/loss_functions.py
@@ -16,7 +16,7 @@ def mean_square_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     -------
     MSE of given predictions
     """
-    raise NotImplementedError()
+    return np.linalg.norm(y_true - y_pred)
 
 
 def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: bool = True) -> float:
